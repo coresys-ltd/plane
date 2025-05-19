2cbbd6b5d29f72314ebb06f304a99eed08f64233
coresys-ltd <136073838+coresys-ltd@users.noreply.github.com>
2025-05-19 14:23:25 +0100

    Update .eslintrc.js

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,6 +1,12 @@
 /** @type {import("eslint").Linter.Config} */
+const path = require("path");
+
 module.exports = {
   root: true,
   extends: ["@plane/eslint-config/next.js"],
   parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: path.resolve(__dirname, "./tsconfig.json"),
+    tsconfigRootDir: __dirname,
+  },
 };
